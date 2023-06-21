0eb788501a892def6853c8909fbffa67db745afb
pavel <piwowarow87@yandex.ru>
2023-06-21 22:21:24 +0300

    HW3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,48 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type wordCounter struct {
+	Word    string
+	Counter int
+}
+
+var sliceWordCounter []wordCounter
+
+func Top10(input string) []string {
+	arr := strings.Fields(input)
+	var result []string
+
+	if len(arr) == 0 {
+		return result
+	}
+	var mapWordCounter = make(map[string]int)
+
+	for _, word := range arr {
+		if _, ok := mapWordCounter[word]; ok {
+			mapWordCounter[word]++
+		} else {
+			mapWordCounter[word] = 1
+		}
+	}
+
+	for word, counter := range mapWordCounter {
+		sliceWordCounter = append(sliceWordCounter, wordCounter{word, counter})
+	}
+
+	sort.Slice(sliceWordCounter, func(idx, idxN int) bool {
+		if sliceWordCounter[idx].Counter == sliceWordCounter[idxN].Counter {
+			return sliceWordCounter[idx].Word < sliceWordCounter[idxN].Word
+		}
+		return sliceWordCounter[idx].Counter > sliceWordCounter[idxN].Counter
+	})
+
+	for _, wc := range sliceWordCounter {
+		result = append(result, wc.Word)
+	}
+
+	return result[0:10]
 }

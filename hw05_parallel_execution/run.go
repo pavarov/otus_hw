99fd9631ddcf1db84c7a8fea3695f4f257ea3a4a
pavel <piwowarow87@yandex.ru>
2023-07-18 20:18:57 +0300

    HW5 refactoring

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,7 +10,6 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	if m <= 0 {
 		return ErrErrorsLimitExceeded
@@ -25,7 +24,14 @@ func Run(tasks []Task, n, m int) error {
 	for i := 0; i < n; i++ {
 		go func() {
 			defer wg.Done()
-			consumer(ch, &errCounter, maxErrors)
+			for task := range ch {
+				if err := task(); err != nil {
+					// avoid race conditions
+					if atomic.AddInt32(&errCounter, 1) > maxErrors {
+						return
+					}
+				}
+			}
 		}()
 	}
 	wg.Wait()
@@ -38,23 +44,9 @@ func Run(tasks []Task, n, m int) error {
 
 func producer(tasks []Task) chan Task {
 	ch := make(chan Task, len(tasks))
-
-	go func() {
-		for _, task := range tasks {
-			ch <- task
-		}
-		defer close(ch)
-	}()
-	return ch
-}
-
-func consumer(ch chan Task, errCounter *int32, maxErrors int32) {
-	for task := range ch {
-		if err := task(); err != nil {
-			// avoid race conditions
-			if atomic.AddInt32(errCounter, 1) > maxErrors {
-				return
-			}
-		}
+	defer close(ch)
+	for _, task := range tasks {
+		ch <- task
 	}
+	return ch
 }

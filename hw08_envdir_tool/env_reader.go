79d484b705c5093feaed28cae1d519163ea3ff04
pavel <piwowarow87@yandex.ru>
2023-08-26 21:19:37 +0300

    HW8 done

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +17,64 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+func closeFileDesc(f *os.File) {
+	if errClose := f.Close(); errClose != nil {
+		log.Fatal(errClose)
+	}
+}
+
+func parseEnvFromFile(f *os.File) string {
+	var v string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		v = scanner.Text()
+		break
+	}
+
+	v = strings.TrimRight(v, "\t ")
+	v = strings.ReplaceAll(v, "\x00", "\n")
+	return v
+}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
+	envs := make(Environment)
+	err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			fileEnvName := info.Name()
+
+			if strings.Contains(fileEnvName, "=") {
+				return nil
+			}
+
+			envVal := new(EnvValue)
+			if info.Size() == 0 {
+				envVal.NeedRemove = true
+				envs[fileEnvName] = *envVal
+				return nil
+			}
+
+			f, err := os.Open(path)
+			defer closeFileDesc(f)
+			if err != nil {
+				return err
+			}
+
+			envVal.Value = parseEnvFromFile(f)
+			envVal.NeedRemove = false
+
+			envs[fileEnvName] = *envVal
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
 	// Place your code here
-	return nil, nil
+	return envs, nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args
+	if len(args) < 3 {
+		log.Fatal("go-envdir usage: min 2 arguments (env dir, command)")
+	}
+
+	envs, err := ReadDir(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	RunCmd(args[2:], envs)
 }

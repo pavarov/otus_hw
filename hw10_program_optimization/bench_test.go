76d8704add8e022dc580520e9948eba79f6d2620
pavel <piwowarow87@yandex.ru>
2023-09-12 00:31:42 +0300

    HW10 done

---
+++ hw10_program_optimization/bench_test.go
@@ -0,0 +1,31 @@
+package hw10programoptimization
+
+import (
+	"archive/zip"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func BenchmarkGetDomainStat(b *testing.B) {
+	b.StopTimer()
+
+	r, err := zip.OpenReader("testdata/users.dat.zip")
+	require.NoError(b, err)
+	defer func(r *zip.ReadCloser) {
+		err := r.Close()
+		if err != nil {
+
+		}
+	}(r)
+
+	require.Equal(b, 1, len(r.File))
+
+	data, err := r.File[0].Open()
+	require.NoError(b, err)
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = GetDomainStat(data, "biz")
+	}
+}
